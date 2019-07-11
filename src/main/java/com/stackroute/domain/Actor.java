1603e364b57f55a3f9e4eadb1c458cbfd82c3ef3
harika <bodduharika2@gmail.com>
2019-07-11 17:56:21 +0530

    task2

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -4,6 +4,11 @@ public class Actor {
     String name;
     String gender;
     int age;
+    public Actor( String name, String gender, int age){
+         this.name=name;
+         this.gender=gender;
+         this.age=age;
+     }
 
 
     public void setGender(String gender) {
