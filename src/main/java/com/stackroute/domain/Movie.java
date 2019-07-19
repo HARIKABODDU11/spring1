6823856dc02d2a9bb9b1efeb8525ff9bd376a7db
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-07-19 12:07:52 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,7 @@
 package com.stackroute.domain;
 
 public class Movie {
+    //Movie “has a” Actor that can be initialized via the corresponding setter method.
     Actor actor;
 
     public Movie(){ }
